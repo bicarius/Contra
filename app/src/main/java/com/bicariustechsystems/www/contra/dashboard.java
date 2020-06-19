ab89be0b99ff7160bec1d0380693ed3ed81dff7d
Davis Makori <bicariusystems@gmail.com>
2020-06-19 19:18:30 +0300

    First Version

---
+++ app/src/main/java/com/bicariustechsystems/www/contra/dashboard.java
@@ -0,0 +1,21 @@
+package com.bicariustechsystems.www.contra;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+        getSupportActionBar().setElevation(0);
+        View view = getSupportActionBar().getCustomView();
+    }
+}
---
+++ app/src/main/java/com/bicariustechsystems/www/contra/launch.java
@@ -0,0 +1,15 @@
+package com.bicariustechsystems.www.contra;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class launch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
